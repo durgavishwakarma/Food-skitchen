408046fc4a1efb3fab94fb767d93faa83c2ac9fd
Durga <durgavishwakarma2009@gmail.com>
2022-06-26 02:26:21 -0700

    Food's Kitchen

---
+++ src/App.js
@@ -0,0 +1,82 @@
+// main page//
+/* 
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+      <header className="App">
+  
+    Food's Kitchen
+  
+  </header>
+  <div className="welcome">
+   welcome to Food's kitchen 
+   </div>
+   <div className="btn">
+    <button > Go to menu</button>
+   </div>
+  </div>
+  );
+}
+
+export default App*/
+
+//food item 
+/*
+import React from 'react';
+import './App.css';
+
+import img1 from './image.jpg';
+
+function Fooditem() {
+  return (
+    <>
+      <div>
+        <header className="App">
+
+          Food's Kitchen
+
+        </header>
+      </div>
+
+      <div className="container">
+
+        <img src={img1} alt="" />
+        <div> burger</div>
+       <div>price-200</div>  
+       <div>
+        <button>
+          +
+        </button>
+        <button>
+          -
+        </button>
+       </div>
+      </div>
+      <div className="container1">
+
+        <img src={img1} alt="" />
+        <div> 
+          food 
+        </div>
+<div> price-200</div>
+<button>
+          +
+        </button>
+        <button>
+          -
+        </button>
+      </div>  
+      <div className="container2">
+
+    
+      </div>
+
+
+    </>
+  );
+}
+
+export default Fooditem*/
---
+++ src/Fooditem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function Fooditem() {
+  return (
+   <div>
+      <header className="App">
+  
+    Food's Kitchen
+  
+  </header>
+  <div className="welcome">
+   welcome to Food's kitchen 
+   </div>
+   <div className="btn">
+    <button > Go to menu</button>
+   </div>
+  </div>
+  );
+}
+
+export default Fooditem;
